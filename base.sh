760e589b97021d5ba54e3ffdd36d960c9ac2cd9d
adampats <adamthepatterson@gmail.com>
2015-12-03 23:04:15 -0800

    forgot jq

--- base.sh
+++ base.sh
@@ -5,7 +5,7 @@ export COMPOSE_RELEASE='1.5.1'
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y curl wget git nmap dstat sl build-essential
+sudo apt-get install -y curl wget git nmap dstat sl build-essential jq
 
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list'
